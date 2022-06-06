56df8efc09e48204a1a367c8207bbac914f5013f
SeonAh Shin <49391145+Se0nAh@users.noreply.github.com>
2022-06-06 17:21:37 +0900

    feat: sunset 페이지

--- sunset/sunset.js
+++ sunset/sunset.js
@@ -1,23 +1,21 @@
 const scene = document.getElementById('scene')
 const bg = document.getElementById('background')
-const cloud1 = document.getElementById('cloud')
+const cloud1 = document.getElementById('cloud1')
+const cloud2 = document.getElementById('cloud2')
+const tree = document.getElementById('tree')
 console.log(scene)
 
-const maxXRange = [-200, 200]
-
 window.addEventListener('mousemove', (e) => {
-    console.log(scene.offsetWidth)
-    const p = scene.offsetWidth / 360
-    scene.style.filter = `hue-rotate(${(e.clientX - scene.offsetLeft)/p}deg)`
-    console.log(e.clientX - scene.offsetLeft)
+    const p = scene.offsetWidth * scene.offsetHeight / 360
+    scene.style.filter = `hue-rotate(${(e.clientX - scene.offsetLeft) * (e.clientY - scene.offsetTop) / p}deg)`
 
-    const offsetCenterX = e.clientX - scene.offsetLeft - scene.offsetWidth/2
-    const offsetCenterY = e.clientY - scene.offsetTop - scene.offsetHeight/2
-    console.log(offsetCenterX)
+    const offsetCenterX = e.clientX - scene.offsetLeft - scene.offsetWidth / 2
+    const offsetCenterY = e.clientY - scene.offsetTop - scene.offsetHeight / 2
 
-    console.log(offsetCenterX/10)
-    bg.style.transform = `scale(1.1) translateX(${-offsetCenterX/200}px) rotateY(${offsetCenterX/200}deg) translateY(${-offsetCenterY/200}px) `
-    cloud1.style.transform = `translateX(${-offsetCenterX/50}px) translateY(${-offsetCenterY/50}px)`
+    bg.style.transform = `scale(1.1) translateX(${-offsetCenterX / 200}px) rotateY(${offsetCenterX / 200}deg) translateY(${-offsetCenterY / 200}px) `
+    cloud1.style.transform = `translateX(${-offsetCenterX / 100}px) translateY(${-offsetCenterY / 100}px)`
+    cloud2.style.transform = `translateX(${-offsetCenterX / 30}px) translateY(${-offsetCenterY / 80}px)`
+    tree.style.transform = `scaleX(1.15) rotateY(${offsetCenterX / 100}deg) translateX(${-offsetCenterX / 20}px)`
 
 })
 
